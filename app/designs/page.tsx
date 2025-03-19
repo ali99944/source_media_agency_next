e31845a343014f71ad22284dbeda7af29f534677
ali99944 <alitarek99944@gmail.com>
2025-03-19 03:02:50 +0200

    update

--- app/designs/page.tsx
+++ app/designs/page.tsx
@@ -328,7 +328,7 @@ export default function DesignsPage() {
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
               {design_showcases.map((project, index) => (
-                <Link key={project.id} href={`/designs/${project.design_service.name}`}>
+                <Link key={project.id} href={`/designs/${project.design_service.page_code}`}>
                   <motion.div
                   initial={{ opacity: 0, y: 20 }}
                   whileInView={{ opacity: 1, y: 0 }}
